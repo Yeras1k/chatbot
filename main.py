d5ace955f70700bf2bc7d9c91475dbbaf12ee67a
Yeras1k <yerasylhello@gmail.com>
2022-11-15 19:44:37 +0600

    server.route

--- main.py
+++ main.py
@@ -31,3 +31,16 @@ def start(message):
     if not result:
         mycursor.execute(f"INSERT INTO users(teleid, name, isActive, isWant) VALUES ({user_id}, {user_name}, False, False)")
         mydb.commit()
+
+@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+def redirect_message():
+    json_string = request.get_data().decode("utf-8")
+    update = telebot.types.Update.de_json(json_string)
+    bot.process_new_updates([update])
+    return "!", 200
+
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
