89ae5513fd4923430eea52476b22d74cc483b546
Yeras1k <yerasylhello@gmail.com>
2022-11-15 18:03:30 +0600

    /start

--- config.py
+++ config.py
@@ -1,3 +1,6 @@
 BOT_TOKEN = "5615146535:AAGYfRt9rDlyKqhLcbytRqyydXlytYVLWCE"
 APP_URL = "https://chatbot-production-f375.up.railway.app/" + BOT_TOKEN
 DB_URI = "mysql://root:IOeQHj8jE6io4OQYTh3T@containers-us-west-103.railway.app:6293/railway"
+PASS = "IOeQHj8jE6io4OQYTh3T"
+HOST = "containers-us-west-103.railway.app"
+USER = "root"
--- main.py
+++ main.py
@@ -12,10 +12,22 @@ logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
 mydb = mysql.connector.connect(
-    host = "containers-us-west-103.railway.app",
+    host = HOST,
     port = "6293",
-    user = "root",
-    password = "IOeQHj8jE6io4OQYTh3T",
+    user = USER,
+    password = PASS,
     database = "railway"
 )
 mycursor = mydb.cursor()
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    user_id = message.from_user.id
+    user_name = message.from_user.username
+    bot.reply_to(message, f"Hello, {message.from_user.first_name}!")
+    bot.send_message(message.chat.id, 'Нажмите "Начать", чтоб начать общение')
+    mycursor.execute(f"SELECT teleid FROM users WHERE teleid = {user_id}")
+    result = db_object.fetchone()
+    if not result:
+        mycursor.execute(f"INSERT INTO users(teleid, name, isActive, isWant) VALUES (%i, %s, %b, %b)", (user_id, user_name, "False", "False")
+        mydb.commit()
