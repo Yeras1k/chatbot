75212b4a271e144cddc120e3aaa9e8109d49daae
Yeras1k <yerasylhello@gmail.com>
2022-11-16 22:28:53 +0600

    ok

--- database.py
+++ database.py
@@ -11,6 +11,15 @@ mydb = mysql.connector.connect(
 mycursor = mydb.cursor()
 
 class Database:
+    def __init__(self):
+        self.connection = self.connect(
+            host = os.environ.get('MYSQLHOST'),
+            port = os.environ.get('MYSQLPORT'),
+            user = os.environ.get('MYSQLUSER'),
+            password = os.environ.get('MYSQLPASSWORD'),
+            database = os.environ.get('MYSQLDATABASE')
+        )
+        self.cursor = self.connection.cursor()
     def add_queue(self, chat_id):
         mycursor.execute(f"INSERT INTO users(teleid) VALUES (%i)", (chat_id))
         mydb.commit()
