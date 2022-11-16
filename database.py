e28438fcf8bea53a95faf96ae8c9a703618bf212
Yeras1k <yerasylhello@gmail.com>
2022-11-16 22:12:51 +0600

    global 2

---
+++ database.py
@@ -0,0 +1,15 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host = os.environ.get('MYSQLHOST'),
+    port = os.environ.get('MYSQLPORT'),
+    user = os.environ.get('MYSQLUSER'),
+    password = os.environ.get('MYSQLPASSWORD'),
+    database = os.environ.get('MYSQLDATABASE')
+)
+mycursor = mydb.cursor()
+
+class Database:
+    def add_queue(self, chat_id):
+        mycursor.execute(f"INSERT INTO users(teleid) VALUES (%i)", (chat_id))
+        mydb.commit()
--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import telebot
 import logging
 import random
-import mysql.connector
+from databse import Database
 from telebot import types
 from config import *
 from flask import Flask, request
@@ -12,82 +12,28 @@ server = Flask(__name__)
 logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
-mydb = mysql.connector.connect(
-    host = os.environ.get('MYSQLHOST'),
-    port = os.environ.get('MYSQLPORT'),
-    user = os.environ.get('MYSQLUSER'),
-    password = os.environ.get('MYSQLPASSWORD'),
-    database = os.environ.get('MYSQLDATABASE')
-)
-mycursor = mydb.cursor()
-
 @bot.message_handler(commands=["start"])
 def start(message):
+    user_name = message.from_user.first_name
     service = telebot.types.ReplyKeyboardMarkup(True, True)
-    service.row('Начать', 'Отмена')
-    user_id = message.from_user.id
-    user_name = message.from_user.username
-    mycursor.execute(f"SELECT teleid FROM users WHERE teleid = {user_id}")
-    result = mycursor.fetchone()
-    if not result:
-        mycursor.execute(f"INSERT INTO users(teleid, username, isActive, isWant, chat, temp) VALUES ({user_id}, '{user_name}', False, False, 0, 0)")
-        mydb.commit()
-        bot.send_message(message.from_user.id, 'Плохо')
-    else:
-        mycursor.execute(f"UPDATE users SET isWant = False, chat = 0, temp = 0 WHERE teleid = {user_id}")
-        mydb.commit()
-        bot.send_message(message.from_user.id, 'Хорошо')
-    send = bot.send_message(message.chat.id, f"Hello, {message.from_user.first_name}! Нажмите Начать, чтоб начать общение", reply_markup=service)
-    bot.register_next_step_handler(send, chatting)
+    service.row('Поиск собеседника')
+    bot.send_message(message.chat.is, f"Привет, {user_name}! Это анонимный чат бот. Нажмите кнопку ниже, чтоб начать поиск собеседника", reply_markup = service)
 
+@bot.message_handler(commands=["menu"])
+def menu(message):
+    service = telebot.types.ReplyKeyboardMarkup(True, True)
+    service.row('Поиск собеседника')
+    bot.send_message(message.chat.id, 'Меню'.format(message.from_user), reply_markup = service)
 
-def chatting(message):
-    if message.text == "Начать":
-        user_id = message.from_user.id
-        bot.send_message(message.from_user.id, 'Ищем...')
-        mycursor.execute(f"UPDATE users SET isWant = True WHERE teleid = {user_id}")
-        mydb.commit()
-        mycursor.execute(f"SELECT teleid FROM users WHERE teleid NOT IN ({user_id}) AND isActive = 0 AND isWant = 1 AND chat = 0")
-        people = mycursor.fetchall()
-        bot.send_message(message.from_user.id, f'{str(people)}')
-        if not people:
+@bot.message_handler(content_types=["text"])
+def bot_message(message):
+    if message.chat.type == 'private':
+        if message.text == 'Поиск собеседника':
             service = telebot.types.ReplyKeyboardMarkup(True, True)
-            service.row('Начать')
-            send = bot.send_message(message.chat.id, f"Собеседник не найден! Попробуйте снова", reply_markup=service)
-            bot.register_next_step_handler(send, chatting)
-        else:
-            service = telebot.types.ReplyKeyboardMarkup(True, True)
-            service.row('Закончить')
-            global person
-            person = random.choice(people[0])
-            mycursor.execute(f"UPDATE users SET temp = {person} WHERE teleid = {user_id}")
-            mydb.commit()
-            mycursor.execute(f"SELECT temp FROM users WHERE teleid = {person}")
-            second = mycursor.fetchone()
-            if second != user_id:
-                mycursor.execute(f"UPDATE users SET temp = {user_id} WHERE teleid = {person}")
-                mycursor.execute(f"UPDATE users SET chat = {person} WHERE teleid = {user_id}")
-                mycursor.execute(f"UPDATE users SET chat = {user_id} WHERE teleid = {person}")
-                mydb.commit()
-                send = bot.send_message(message.from_user.id, 'Можете писать')
-                bot.register_next_step_handler(send, chat)
-            else:
-                mycursor.execute(f"UPDATE users SET chat = {person} WHERE teleid = {user_id}")
-                mycursor.execute(f"UPDATE users SET chat = {user_id} WHERE teleid = {person}")
-                mydb.commit()
-                send = bot.send_message(message.from_user.id, 'Можете писать', reply_markup=service)
-                bot.register_next_step_handler(send, chat)
+            service.row('Остановить поиск')
 
-
-def chat(message):
-    bot.send_message(message.from_user.id, 'Ты в чате')
-    if message.text == "Закончить":
-        msg = bot.send_message(person, "Собеседник решил закончить разговор! Нажмите Закончить")
-        bot.register_next_step_handler(msg, start)
-    else:
-        msg = bot.send_message(person, message.text)
-        bot.register_next_step_handler(msg, chat)
-
+            add_queue(message.chat.id)
+            bot.send_message(message.chat.id, 'Идет поиск', reply_markup = service)
 
 @server.route(f"/{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
