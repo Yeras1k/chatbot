3b0209e696be8e5a407dfaf3c597b6ad386dd8a2
Yeras1k <yerasylhello@gmail.com>
2022-11-16 16:20:32 +0600

    BOT_TOKEN = os.environ.get('BOT_TOKEN')

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-BOT_TOKEN = RAILWAY_BOT_TOKEN
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
 APP_URL = "https://chatbot-production-f375.up.railway.app/" + BOT_TOKEN
 DB_URI = "mysql://root:pla4qKerLWls1m87aKAa@containers-us-west-115.railway.app:5990/railway"
