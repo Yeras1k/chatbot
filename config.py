9d99cbdc5810eb1cca591c3cbd79345af778f285
Yeras1k <yerasylhello@gmail.com>
2022-11-16 15:22:25 +0600

    secret bot token 1

--- config.py
+++ config.py
@@ -1,3 +1,2 @@
-BOT_TOKEN = "5615146535:AAGYfRt9rDlyKqhLcbytRqyydXlytYVLWCE"
 APP_URL = "https://chatbot-production-f375.up.railway.app/" + BOT_TOKEN
 DB_URI = "mysql://root:pla4qKerLWls1m87aKAa@containers-us-west-115.railway.app:5990/railway"
