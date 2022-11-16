1b7b515959b285acd05c644fb4d3b4199622bba3
Yeras1k <yerasylhello@gmail.com>
2022-11-16 15:08:29 +0600

    2

--- config.py
+++ config.py
@@ -1,2 +1,2 @@
-APP_URL = "https://chatbot-production-f375.up.railway.app/" + BOT_TOKEN
+APP_URL = "https://chatbot-production-f375.up.railway.app/" + secrets.BOT_TOKEN
 DB_URI = "mysql://root:pla4qKerLWls1m87aKAa@containers-us-west-115.railway.app:5990/railway"
